3701a1a64cb9ce8ac469e25e66cf72299377d535
drus197 <devonrusso21@gmail.com>
2024-08-06 18:17:47 -0400

    initial commit

--- ml_model/model.py
+++ ml_model/model.py
@@ -4,18 +4,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
 
 # read csv file
 dataset = pd.read_csv('/data/bot_detection_data.csv')
 
 # Split independent and dependant variables
-x = dataset.iloc[:, 3:7]
+X = dataset.iloc[:, 3:7]
 y = dataset.iloc[:, 7]
 
 # Split data into training and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 # Perform feature scaling on the dataset
+# Not necessary with decision tree algorithm
 sc = StandardScaler()
-x_train.iloc[:, :3] = sc.fit_transform(x_train.iloc[:, :3])
-x_test.iloc[:, :3] = sc.transform(x_test.iloc[:, :3])
+X_train.iloc[:, :3] = sc.fit_transform(X_train.iloc[:, :3])
+X_test.iloc[:, :3] = sc.transform(X_test.iloc[:, :3])
+
+# Train the model
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
