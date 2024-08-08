058353a639b944e720bc0d082b0069c027e75a8b
drusso97 <devonrusso21@gmail.com>
2024-08-08 09:34:05 -0400

    fixed inaccuracy by modifying regressor variables

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Train the model
 regressor = DecisionTreeRegressor(random_state=0)
-regressor.fit(X_train, y_train)
+regressor.fit(X, y)
 
 # Make predictions
 y_pred = regressor.predict(X_test)
